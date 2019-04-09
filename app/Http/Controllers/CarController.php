33b809d299c8500d46544f3bf673b84191e38bfb
Facundo Migliorini <facundo.migliorini@simplex.software>
2019-04-09 11:30:43 -0300

    First commit for car controller

---
+++ app/Http/Controllers/CarController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Car;
+
+class CarController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $cars = Car::all();
+        return response()->json($cars);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        // some code here...
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $car = new Car([
+            'name' => $reqiest->get('name'),
+            'engine' => $request->get('engine')
+        ]);
+        $car->save();
+
+        return response()->json('Car added');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $car = Car::find($id);
+        return response()->json($car);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $car = Car::find($id);
+        $car->name = $request->get($name);
+        $car->engine = $request->get($engine);
+        $car->save();
+
+        return response()->json('Car updated');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $car = Car::find($id);
+        $car->delete();
+
+        return response()->json('Car Deleted');
+    }
+}
